8b83be3b764a6d9390ad86d3a1e45105b18a83e1
Pontus Carlsson <pontus_carlsson@hotmail.com>
2019-08-27 20:22:55 +0200

    Styling

--- src/Main.js
+++ src/Main.js
@@ -8,34 +8,40 @@ import StartScreen from './StartScreen';
 function Main() {
   const [data, setData] = useState([]);
   const [started, setStarted] = useState(false);
+  const [timer, setTimer] = useState(false);
   const [saveFile, setSaveFile] = storage.useLocalStorage('save', undefined);
 
-  useEffect(() => loadGame(), []);
+  useEffect(() => {
+    const loadGame = () => {
+      if (saveFile) {
+        setData(saveFile);
+      } else {
+        api.loadGame().then(response => {
+          setData(response.data);
+          setSaveFile(response.data);
+        });
+      }
+    };
 
-  const loadGame = () => {
-    if (saveFile) {
-      setData(saveFile);
-    } else {
-      api.loadGame().then(response => {
-        setData(response.data);
-        setSaveFile(response.data);
-      });
-    }
-  };
+    setTimeout(() => {
+      setTimer(true);
+    }, 1850);
+
+    loadGame();
+  }, []);
 
   console.log('data', data);
+  if (!data) return <div className="center">Loading..</div>;
   return (
     <div className="center" style={{ height: '250px' }}>
-      <StartScreen started={started} data={data}></StartScreen>
-      {!started && (
-        <div
-          style={{ backgroundColor: 'Start', marginTop: '13px' }}
-          onClick={() => setStarted(true)}
-        >
-          Start Game
-        </div>
-      )}
-      {started && <div>{data.description}</div>}
+      <StartScreen
+        started={started}
+        data={data}
+        timer={timer}
+        onStarted={setStarted}
+      ></StartScreen>
+
+      {started && <div className="textWindow">{data.description}</div>}
     </div>
   );
 }
--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import './app.css';
 
-export default function StartScreen({ started, data }) {
-  return !started && <div className="textWindow">{data.message}</div>;
+export default function StartScreen({ started, data, timer, onStarted }) {
+  return (
+    !started && (
+      <div
+        className="textWindow"
+        style={{
+          height: '60px',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        {data.message}
+        {!started && timer && (
+          <div
+            className="center"
+            style={{
+              marginTop: '15px',
+            }}
+            onClick={() => onStarted(true)}
+          >
+            <div>Start Game</div>{' '}
+          </div>
+        )}
+      </div>
+    )
+  );
 }
