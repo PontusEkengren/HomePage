6c846c578e0bf808d7075f46a404e061b2c247d6
Pontus Carlsson <pontus_carlsson@hotmail.com>
2019-10-08 21:37:33 +0200

    Add some buttons

--- src/DialogText.js
+++ src/DialogText.js
@@ -1,18 +1,36 @@
 import React from 'react';
-import * as dialog from './dialog';
-import Map from './Maze';
+// import * as dialog from './dialog';
+import './arrows.css';
+import Key from './Key';
 
 export default function DialogText({ data, move, onHandleClick }) {
   return (
     <div className="center">
       <div className="dialog">{data.description}</div>
       {data && data.exits && (
-        <div>
+        <div className="arrowKeyNorthSouth">
           {data.exits.map((exit, index) => (
-            <div key={index} onClick={() => onHandleClick(exit)}>
-              {dialog.direction[exit]}
-            </div>
+            // <div key={index} onClick={() => }>
+            //   {dialog.direction[exit]}
+            // </div>
+
+            <Key key={index} arrow={exit} handleClick={onHandleClick}></Key>
           ))}
+
+          {/* <div className="test">
+            <i className="arrow up"></i>
+            <div>North</div>
+          </div>
+          <div className="arrowKeyWestEast">
+            <div>
+              <i className="arrow left"></i> West
+            </div>
+            <div>South</div>
+            <div>
+              East <i className="arrow right"></i>
+            </div>
+          </div>
+          <i className="arrow down"></i> */}
         </div>
       )}
     </div>
---
+++ src/Key.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './arrows.css';
+
+export default function Key({ arrow, handleClick }) {
+  switch (arrow) {
+    case 'N':
+      return (
+        <div className="centerArrow" onClick={() => handleClick('N')}>
+          <i className="arrow up"></i>
+          <div>North</div>
+        </div>
+      );
+    case 'W':
+      return (
+        <div className="centerArrow" onClick={() => handleClick('W')}>
+          <div>West</div> <i className="arrow left"></i>
+        </div>
+      );
+    case 'E':
+      return (
+        <div className="centerArrow" onClick={() => handleClick('E')}>
+          <div>East</div> <i className="arrow right"></i>
+        </div>
+      );
+    case 'S':
+      return (
+        <div className="centerArrow" onClick={() => handleClick('S')}>
+          <div>South</div>
+          <i className="arrow down"></i>
+        </div>
+      );
+    default:
+      return null;
+  }
+  return null;
+}
