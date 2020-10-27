99bf2f3fefeb7699fe575af0c26cda8e408a8de7
carlssonn <pontus_carlsson@hotmail.com>
2020-10-27 09:58:17 +0100

    npm update

--- src/Main.js
+++ src/Main.js
@@ -5,6 +5,7 @@ import * as api from './api';
 import * as storage from './storage';
 import StartScreen from './StartScreen';
 import DialogText from './DialogText';
+import GoogleLogin from 'react-google-login';
 
 function Main() {
   const [data, setData] = useState([]);
@@ -147,6 +148,16 @@ function Main() {
         </div>
       )}
       <div />
+      <GoogleLogin
+        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+        render={renderProps => (
+          <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+        )}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
       {/* <div>
         <div >
           <div>Header</div>
