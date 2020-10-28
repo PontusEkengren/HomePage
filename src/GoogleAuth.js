ef19d5eb462df8fc598689c8ada6da84365f9feb
carlssonn <pontus_carlsson@hotmail.com>
2020-10-28 09:58:05 +0100

    Refactor to use styled components for GoogleAuth

--- src/GoogleAuth.js
+++ src/GoogleAuth.js
@@ -1,31 +1,37 @@
 import React from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { Image, ContainerSpaceBetween } from './Styled/default';
 import './app.css';
 
-export default function GoogleAuth({ imageUrl, isLogined, clientId, handleSuccess, onLogoutSuccess, onLogOutFailure, onLogInFailure }) {
-
+export default function GoogleAuth({
+  imageUrl,
+  isLogined,
+  clientId,
+  handleSuccess,
+  onLogoutSuccess,
+  onLogOutFailure,
+  onLogInFailure,
+}) {
   return (
-    <div style={{ display: "flex", justifyContent: "space-between", alignItems: 'center', width: "160px" }}>
-
-      {imageUrl &&
-        //TODO: Change to styled component
-        // eslint-disable-next-line jsx-a11y/alt-text
-        <img style={{ marginRight: '12px', width: '42px', height: '42px' }} src={imageUrl} />
-      }
-      {isLogined ?
+    <ContainerSpaceBetween>
+      {imageUrl && <Image src={imageUrl} />}
+      {isLogined ? (
         <GoogleLogout
           clientId={clientId}
-          buttonText='Logout'
+          buttonText="Logout"
           onLogoutSuccess={onLogoutSuccess}
           onFailure={onLogOutFailure}
-        /> : <GoogleLogin
+        />
+      ) : (
+        <GoogleLogin
           clientId={clientId}
-          buttonText='Login'
+          buttonText="Login"
           onSuccess={handleSuccess}
           onFailure={onLogInFailure}
           cookiePolicy={'single_host_origin'}
-          responseType='code,token'
-        />}
-    </div>
+          responseType="code,token"
+        />
+      )}
+    </ContainerSpaceBetween>
   );
 }
---
+++ src/Styled/default.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Image = styled.img`
+  margin-right: 12px;
+  width: 42px;
+  height: 42px;
+`;
+
+export const ContainerSpaceBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 160px;
+`;
